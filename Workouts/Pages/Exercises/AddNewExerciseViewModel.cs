f0334a8b236ce51a01398f74f84fecb9388c5f8f
francesco-g-giuneco <francesco.giancristofaro@giuneco.it>
2023-06-18 14:32:34 +0200

    wip

--- Workouts/Pages/Exercises/AddNewExerciseViewModel.cs
+++ Workouts/Pages/Exercises/AddNewExerciseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using WorkoutsApp.Constants;
 using WorkoutsApp.Models.DB;
 using WorkoutsApp.Services;
 
@@ -8,49 +9,48 @@ namespace WorkoutsApp.Pages.Exercises
     public partial class AddNewExerciseViewModel : BaseViewModel
     {
         private readonly IExerciseService _exerciseService;
-        private readonly ICategoriesService _categoriesService;
         [ObservableProperty] string _exerciseName;
         [ObservableProperty] string _description = string.Empty;
-        [ObservableProperty] ExerciseCategories _selectedCategory;
-        [ObservableProperty] IEnumerable<ExerciseCategories> _categories;
+        [ObservableProperty] string _selectedCategory;
+        [ObservableProperty] IList<string> _categories;
 
         [RelayCommand]
         async void SaveExercise()
         {
-            //try
-            //{
-            //    IsBusy = true;
-            //    if (string.IsNullOrEmpty(Name) || SelectedCategory is ExerciseCategory.NoCategory)
-            //    {
-            //        await Shell.Current.DisplayAlert("Attenzione", "Campi nome e categoria sono obbligatori", "OK");
-            //        return;
-            //    }
+            try
+            {
+                IsBusy = true;
+                var parseRes = Enum.TryParse(SelectedCategory, out ExerciseCategory category);
+                if (string.IsNullOrEmpty(ExerciseName) || !parseRes || category is ExerciseCategory.NoCategory)
+                {
+                    await Shell.Current.DisplayAlert("Attenzione", "Campi nome e categoria sono obbligatori", "OK");
+                    return;
+                }
 
-            //    var ex = new Exercise() { Name = Name, Description = Description, Category = SelectedCategory };
-            //    await _exerciseService.Insert(ex);
+                var ex = new Exercise() { Name = ExerciseName, Description = Description, Category = category };
+                await _exerciseService.Insert(ex);
 
-            //    await Shell.Current.GoToAsync("..");
+                await Shell.Current.GoToAsync("..");
 
-            //}
-            //catch (Exception ex)
-            //{
-            //    await ManageException(ex);
-            //}
-            //finally
-            //{
-            //    IsBusy = false;
-            //}
+            }
+            catch (Exception ex)
+            {
+                await ManageException(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
         protected override async Task Appearing()
         {
-            Categories = await _categoriesService.GetAll();
+            Categories = Enum.GetValues(typeof(ExerciseCategory)).Cast<ExerciseCategory>().Select(x=>x.ToString()).ToList();
         }
 
-        public AddNewExerciseViewModel(IExerciseService exerciseService,ICategoriesService categoriesService)
+        public AddNewExerciseViewModel(IExerciseService exerciseService)
         {
             _exerciseService = exerciseService;
-            _categoriesService = categoriesService;
         }
 
     }
